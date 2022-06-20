8b2cd94f25407a177a9ca62465a24ed52713adae
daingo_linux <daingo7211@gmail.com>
2022-06-20 12:18:36 +0700

    fix morgan

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const route = require('./routes');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const app = express();
 
 // Middleware
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.json());
 app.use(
   express.urlencoded({
